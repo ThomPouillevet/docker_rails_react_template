4e1c29d99d32036e19e65726e39e1a091213392a
Thomas Pouillevet <thomas.pouillevet@gmail.com>
2020-06-07 19:00:17 +0200

    ♻️ Cleans code

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="Mahault" />,
+    <Hello name="React" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
